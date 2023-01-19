dcfb5cfae41891f2523955455bf978b8960c5be8
Carl Gützkow <cjgutzkow@gmail.com>
2023-01-19 08:34:32 +0100

    add jest to project

---
+++ Task3/calculator/tests/unit/Calculator.spec.ts
@@ -0,0 +1,7 @@
+import Calculator from '@/components/Calculator.vue'
+
+describe('Calculator', () => {
+  it('Calculator render display with 0 successfully', () => {
+    expect(true).toBe(true)
+  })
+})
