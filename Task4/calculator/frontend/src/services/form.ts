11ca3bfb6e569da010c8ef976fd65812c37393fb
Carl Gützkow <cjgutzkow@gmail.com>
2023-01-29 22:51:03 +0100

    correct endpoint and remove json-server

--- Task4/calculator/frontend/src/services/form.ts
+++ Task4/calculator/frontend/src/services/form.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const postForm = (): Promise<boolean> => {
   return axios
-    .post("http://localhost:3000/data/", {})
+    .post("http://localhost:8080/feedback/", {})
     .then(() => {
       return true;
     })
