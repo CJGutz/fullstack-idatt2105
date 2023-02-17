e8d892b6f5fa425116690344de85737fac3ea8c8
Carl Gützkow <cjgutzkow@gmail.com>
2023-02-17 09:52:16 +0100

    add mediatype to calculator endpoint

--- Task4/calculator/backend/src/main/kotlin/edu/ntnu/idatt2105/calculator/resources/CalculatorResource.java
+++ Task4/calculator/backend/src/main/kotlin/edu/ntnu/idatt2105/calculator/resources/CalculatorResource.java
@@ -4,6 +4,7 @@ import edu.ntnu.idatt2105.calculator.models.Expression;
 import edu.ntnu.idatt2105.calculator.services.CalculatorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ public class CalculatorResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorResource.class);
 
-    @PostMapping("/calculate")
+    @PostMapping(value = "/calculate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public ResponseEntity<String> calculate(@RequestBody Expression body) {
         LOGGER.info("Received: " + body.getExp());
